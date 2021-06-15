80806ae1465e60cb02c8423420c6ff2eff6ed6c7
ryandaiswara <ryandaiswara@gmail.com>
2021-06-16 01:04:12 +0700

    update function.js

--- 01-Javascript/Function.js
+++ 01-Javascript/Function.js
@@ -3,20 +3,29 @@ function panggilNama () {
     console.log('ryanda iswara');
 }
 
+
 //  02. Memanggil function
 panggilNama();
+
+
 
 //  03. Function expression
 const panggilNama2 = function () {
     console.log(`Nama saya adalah ryanda iswara`);
 }
+
 panggilNama2();
+
+
 
 //  04. Funtion dengan parameter
 const panggilNama3 = function (nama) {
     console.log(`Nama saya adalah ${nama}`);
 }
+
 panggilNama3('ryanda iswara');
+
+
 
 //  05. Function dengan default parameter
 const panggilNama4 = function(nama, umur = 28) {
@@ -24,27 +33,39 @@ const panggilNama4 = function(nama, umur = 28) {
 }
 panggilNama4('ryanda iswara');
 
+
+
 //  06. Functin dengan return
 const funcReturn = function (a, b) {
     return a * b;
 }
+
 let hasilPerkalian = funcReturn(2, 3);
 console.log(hasilPerkalian);
+
+
 
 //  07a. Arrow Function tanpa parameter / implicit return
 const arrFunc = () => console.log(`Ini adalah arrow function tanpa parameter`);
 arrFunc();
 
+
+
 //  07b. Arrow funtion dengan satu parameter / implicit return
 const arrFunc2 = nama => console.log(`nama saya adalah ${nama}`);
 arrFunc2('ryanda iswara');
+
+
 
 //  07c. Arrow function dengan lebih dari satu parameter
 const arrFunc3 = (a, b) => {
     return a * b;
 }
+
 let hasilPerkalian2 = arrFunc3(2, 2);
 console.log(hasilPerkalian2);
+
+
 
 //  08. Funtion Recurtion / memanggil dirinya sendiri
 const funcRecur = function (n) {
@@ -56,7 +77,10 @@ const funcRecur = function (n) {
     console.log(`ini adalah perulangan ke ${n}`);
     return funcRecur(n-1); // memanggil dirinya sendiri
 }
+
 funcRecur(5);
+
+
 
 //  09a. Function dengan rest parameter
 //  Menyimpan arguments kedalam array
@@ -69,8 +93,11 @@ const funcRestParam = (...numbers) => {
 
     return total;
 }
+
 let hasilRestParam = funcRestParam(1, 2, 3, 4, 5);
 console.log(hasilRestParam);
+
+
 
 //  09b. Function dengan arguments
 const funcArgum = function () {
@@ -85,6 +112,8 @@ const funcArgum = function () {
 
 let hasilFuncArgum = funcArgum(1, 2, 3, 4, 5);
 console.log(hasilFuncArgum);
+
+
 
 //  10. Callback function
 //  Function yang menjadi parameter dan dipanggil didalam function utama
